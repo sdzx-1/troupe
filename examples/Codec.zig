4af93c0610d43ff1609dbd26c04b7dbe48ec59d0
sdzx-1 <shangdizhixia1993@163.com>
2026-05-07 23:04:38 +0800

    Refect Codec.zig

--- examples/Codec.zig
+++ examples/Codec.zig
@@ -4,8 +4,8 @@ const builtin = @import("builtin");
 const native_endian = builtin.target.cpu.arch.endian();
 
 pub fn encode(writer: *std.Io.Writer, state_id: anytype, val: anytype) !void {
-    const id: u8 = @intFromEnum(state_id);
-    try writer.writeByte(id);
+    const id: u32 = @intFromEnum(state_id);
+    try writer.writeInt(u32, id, native_endian);
     if (@TypeOf(val) == Notify) {
         try writer.writeInt(u32, val.troupe_notify, native_endian);
     } else {
@@ -13,29 +13,7 @@ pub fn encode(writer: *std.Io.Writer, state_id: anytype, val: anytype) !void {
             inline else => |msg, tag| {
                 try writer.writeByte(@intFromEnum(tag));
                 const data = msg.data;
-                switch (@typeInfo(@TypeOf(data))) {
-                    .void => {},
-                    .bool => {
-                        const v: u8 = if (data) 1 else 0;
-                        try writer.writeInt(u8, v, native_endian);
-                    },
-                    .int => {
-                        try writer.writeInt(@TypeOf(data), data, native_endian);
-                    },
-                    .@"struct" => {
-                        try data.encode(writer);
-                    },
-                    .pointer => |p| {
-                        if (p.is_const == true and p.child == u8) {
-                            const len: usize = data.len;
-                            try writer.writeInt(usize, len, native_endian);
-                            try writer.writeAll(data);
-                        } else {
-                            @compileError("Not impl!");
-                        }
-                    },
-                    else => @compileError("Not impl!"),
-                }
+                try encode_anytype(writer, data);
             },
         }
     }
@@ -54,8 +32,8 @@ pub fn TagPayloadByName(comptime U: type, comptime tag_name: []const u8) type {
 }
 
 pub fn decode(reader: *std.Io.Reader, state_id: anytype, T: type) !T {
-    const id: u8 = @intFromEnum(state_id);
-    const rid = try reader.takeByte();
+    const id: u32 = @intFromEnum(state_id);
+    const rid = try reader.takeInt(u32, native_endian);
     if (id != rid and T != Notify) {
         std.debug.print("id: {d}, rid: {d}\n", .{ id, rid });
         return error.IncorrectStatusReceived;
@@ -69,41 +47,72 @@ pub fn decode(reader: *std.Io.Reader, state_id: anytype, T: type) !T {
         switch (tag) {
             inline else => |t| {
                 const Data = @FieldType(TagPayloadByName(T, @tagName(t)), "data");
-                switch (@typeInfo(Data)) {
-                    .void => {
-                        return @unionInit(T, @tagName(t), .{ .data = {} });
-                    },
-                    .bool => {
-                        const data = try reader.takeInt(u8, native_endian);
-                        const bv: bool = switch (data) {
-                            0 => false,
-                            1 => true,
-                            else => unreachable,
-                        };
-                        return @unionInit(T, @tagName(t), .{ .data = bv });
-                    },
-                    .int => {
-                        const data = try reader.takeInt(Data, native_endian);
-                        return @unionInit(T, @tagName(t), .{ .data = data });
-                    },
-
-                    .pointer => |p| {
-                        if (p.is_const == true and p.child == u8) {
-                            const len = try reader.takeInt(usize, native_endian);
-                            const str = try reader.take(len);
-                            return @unionInit(T, @tagName(t), .{ .data = str });
-                        } else {
-                            @compileError("Not impl!");
-                        }
-                    },
-
-                    .@"struct" => {
-                        const data = try Data.decode(reader);
-                        return @unionInit(T, @tagName(t), .{ .data = data });
-                    },
-                    else => @compileError("Not impl!"),
-                }
+                return @unionInit(T, @tagName(t), .{ .data = try decode_type(reader, Data) });
             },
         }
+    }
+}
+
+pub fn encode_anytype(writer: *std.Io.Writer, data: anytype) !void {
+    switch (@typeInfo(@TypeOf(data))) {
+        .void => {},
+        .bool => {
+            const v: u8 = if (data) 1 else 0;
+            try writer.writeByte(v);
+        },
+        .int => {
+            try writer.writeInt(@TypeOf(data), data, native_endian);
+        },
+        .pointer => |p| {
+            if (p.is_const == true and p.child == u8) {
+                const len: usize = data.len;
+                try writer.writeInt(usize, len, native_endian);
+                try writer.writeAll(data);
+            } else {
+                @compileError("Not impl!");
+            }
+        },
+        .@"struct" => |stru| {
+            inline for (stru.fields) |struct_field| {
+                try encode_anytype(writer, @field(data, struct_field.name));
+            }
+        },
+        else => @compileError("Not impl!"),
+    }
+}
+
+pub fn decode_type(reader: *std.Io.Reader, Data: type) !Data {
+    switch (@typeInfo(Data)) {
+        .void => return {},
+        .bool => {
+            const data = try reader.takeByte();
+            const bv: bool = switch (data) {
+                0 => false,
+                1 => true,
+                else => unreachable,
+            };
+            return bv;
+        },
+        .int => {
+            const data = try reader.takeInt(Data, native_endian);
+            return data;
+        },
+        .pointer => |p| {
+            if (p.is_const == true and p.child == u8) {
+                const len = try reader.takeInt(usize, native_endian);
+                const str = try reader.take(len);
+                return str;
+            } else {
+                @compileError("Not impl!");
+            }
+        },
+        .@"struct" => |stru| {
+            var data: Data = undefined;
+            inline for (stru.fields) |struct_field| {
+                @field(data, struct_field.name) = try decode_type(reader, struct_field.type);
+            }
+            return data;
+        },
+        else => @compileError("Not impl!"),
     }
 }
